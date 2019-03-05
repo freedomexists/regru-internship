428699e11951d38fed4bb9283a03661edbf90983
freedom_exists <freedomexists.pas@gmail.com>
2019-03-05 20:03:19 +0300

    Выполнил задание 3

---
+++ task3/task3.py
@@ -0,0 +1,38 @@
+# Программа принимает на вход строку. Эта строка — путь к файлу.
+# В случае существования файла:
+# 1) проверить возможна ли в него запись.
+# 2) удалить, и закончить работу.
+# Если файла нет:
+# 1) создать,
+# 2) записать строку c латинскими и кириллическими буквами в кодировке UTF-8
+# 3) прочитать и вывести содержимое в кодировке WINDOWS-1251 и в кодировке UTF-8
+# 4) удалить, и закончить работу.
+
+
+import sys
+import os
+
+
+if __name__ == '__main__':
+
+    try:
+        pth = sys.argv[1]
+        if os.access(pth, os.F_OK):
+            print('Доступ на запись - {}'.format(os.access(pth, os.R_OK)))
+            os.remove(pth)
+
+        else:
+            with open(pth, 'wb+') as f:
+                f.write('some string строка'.encode('utf-8'))
+                f.seek(0)
+                content = f.read()
+                print(content.decode('WINDOWS-1251'), content.decode('utf-8'), sep='\n')
+            os.remove(pth)
+
+    except IndexError:
+        print('Введите путь до файла в качестве параметра')
+
+
+# $ sudo python3 task3.py /home/test.txt
+# some string СЃС‚СЂРѕРєР°
+# some string строка
