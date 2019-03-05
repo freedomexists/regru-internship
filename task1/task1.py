38940a89caeaa19f6394dc1772ce6332f8f4e073
freedom_exists <freedomexists.pas@gmail.com>
2019-03-05 16:00:57 +0300

    Выполнил задание 1

---
+++ task1/task1.py
@@ -0,0 +1,13 @@
+def find_one_char(char):
+    while True:
+        if len(char) != 1:
+            print('Введите ровно один символ')
+        else:
+            with open('task1_data.txt', 'rb+') as f:
+                content = f.read()
+                return content.count(char.encode('utf-8'))
+
+
+if __name__ == "__main__":
+    char = input('Введите один символ для поиска в тексте: ')
+    print('Символ \'{}\' встечается в тексте {} раз'.format(char, find_one_char(char)))
