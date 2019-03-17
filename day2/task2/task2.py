4c4a1d1fceeb3d97ea030660c63a54c478010bbe
freedom_exists <freedomexists.pas@gmail.com>
2019-03-17 23:54:15 +0300

    Выполнил задание 2

--- day2/task2/task2.py
+++ day2/task2/task2.py
@@ -2,5 +2,20 @@
 # Напишите программу, которая использует список из задачи 1, и складывает все численные данные.
 # Постарайтесь использовать максимально возможное кол-во элементов.
 
+
+def isfloat(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 lst = [1, 5445, 5445.0, 1.56, '1.90', 'сорок_два', False, '', True]
 
+print(sum(elem if isinstance(elem, (int, float))
+          else float(elem) if isinstance(elem, str) and isfloat(elem) else 0 for elem in lst))
+
+
+# $ python3 task2.py
+# 10895.46
