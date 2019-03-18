a542f8e4fb40cb6a73c551dece496016b0f77579
freedom_exists <freedomexists.pas@gmail.com>
2019-03-18 11:20:54 +0300

    Выполнил задание 3

--- day2/task3/task3.py
+++ day2/task3/task3.py
@@ -2,4 +2,9 @@
 # Напишите программу, которая принимает строку цветов, разделённых пробелами.
 # Выведите первый и последний цвета и общее количество уникальных цветов.
 
+colors = input().split()
+print(colors[0], colors[-1], len(set(colors)))
 
+# $ python3 task3.py
+# red green red green black
+# red black 3
