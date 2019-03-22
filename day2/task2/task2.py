59b9e6650be84ef59cca508ff6789e66ce68deee
freedom_exists <freedomexists.pas@gmail.com>
2019-03-22 18:06:09 +0300

    сделал читабельнее

--- day2/task2/task2.py
+++ day2/task2/task2.py
@@ -2,6 +2,8 @@
 # Напишите программу, которая использует список из задачи 1, и складывает все численные данные.
 # Постарайтесь использовать максимально возможное кол-во элементов.
 
+lst = [1, 5445, 5445.0, 1.56, '1.90', 'сорок_два', False, '', True]
+
 
 def is_float(s):
     try:
@@ -11,10 +13,20 @@ def is_float(s):
         return False
 
 
-lst = [1, 5445, 5445.0, 1.56, '1.90', 'сорок_два', False, '', True]
+summ = 0
 
-print(sum(elem if isinstance(elem, (int, float))
-          else float(elem) if isinstance(elem, str) and is_float(elem) else 0 for elem in lst))
+for elem in lst:
+
+    if isinstance(elem, (int, float)):
+        summ += elem
+    elif isinstance(elem, str) and is_float(elem):
+        summ += float(elem)
+
+print(summ)
+
+# В одну строку, но, вроде, не читабельно.
+# print(sum(elem if isinstance(elem, (int, float))
+#           else float(elem) if isinstance(elem, str) and is_float(elem) else 0 for elem in lst))
 
 
 # $ python3 task2.py
