237cf1f107f0fce823965588901a7bf07d23bda3
freedom_exists <freedomexists.pas@gmail.com>
2019-04-01 16:25:52 +0300

    добавил валидацию ввода

--- day2.5/task1/task1.py
+++ day2.5/task1/task1.py
@@ -20,36 +20,43 @@ def e_func(n, i, j):
         return 4 * (n - 2 * j) + 1 + e_func(n, i, j - 1)
 
 
-n = int(input('Введите размер матрицы: '))
-align = len(str(n * n))
+try:
+    n = int(input('Введите размер матрицы: '))
 
-for i in range(n):
+except ValueError:
+    print('Ошибка. Введите число')
 
-    line = []
+else:
 
-    if i <= n // 2:
+    align = len(str(n * n))
 
-        for j in range(i):
-            line.append(w_func(n, i, j))
+    for i in range(n):
 
-        for j in range(n - 2 * i):
-            line.append(4 * (n - i) * i + j + 1)
+        line = []
 
-        for j in range(i - 1, -1, -1):
-            line.append(e_func(n, i, j))
+        if i <= n // 2:
 
-    else:
+            for j in range(i):
+                line.append(w_func(n, i, j))
 
-        for j in range(n - i - 1):
-            line.append(w_func(n, i, j))
+            for j in range(n - 2 * i):
+                line.append(4 * (n - i) * i + j + 1)
 
-        for j in range(n - (n - i - 1) * 2, 0, -1):
-            line.append(4 * i * (n - i - 1) + 2 * n - 2 + j)
+            for j in range(i - 1, -1, -1):
+                line.append(e_func(n, i, j))
 
-        for j in range(n-i-2, -1, -1):
-            line.append(e_func(n, i, j))
+        else:
 
-    print(' '.join(['{:>{}}'.format(line[i], align) for i in range(n)]))
+            for j in range(n - i - 1):
+                line.append(w_func(n, i, j))
+
+            for j in range(n - (n - i - 1) * 2, 0, -1):
+                line.append(4 * i * (n - i - 1) + 2 * n - 2 + j)
+
+            for j in range(n-i-2, -1, -1):
+                line.append(e_func(n, i, j))
+
+        print(' '.join(['{:>{}}'.format(line[i], align) for i in range(n)]))
 
 # Результат:
 # $ python3 task1.py
