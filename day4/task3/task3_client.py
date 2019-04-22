734abd47d391cc0177c56e56c73256f6464a7852
freedom_exists <freedomexists.pas@gmail.com>
2019-04-22 16:06:20 +0300

    task4 done

--- day4/task3/task3_client.py
+++ day4/task3/task3_client.py
@@ -2,8 +2,17 @@ import socket
 import json
 
 s = input('Введите строку в формате "HH[:MM[:SS]]-<some_string>" для поиска: ')
+try:
+    _, _ = s.split('-')
+except ValueError:
+    print('Ошибка ввода')
 sock = socket.socket()
 sock.connect(('127.0.0.1', 8887))
 
-json_req = json.dumps(s.encode())
-sock.send(json_req)
+json_req = json.dumps(s)
+sock.send(json_req.encode('utf-8'))
+size = sock.recv(128).decode('utf-8')
+response = json.loads(sock.recv(int(size)+8).decode('utf-8'))
+
+print('Найдено {} совпадений\n'.format(len(response)), ''.join([line for line in response]), sep='')
+
--- day4/task3/task3_server.py
+++ day4/task3/task3_server.py
@@ -4,13 +4,50 @@
 import socket
 import json
 import datetime
+from operator import itemgetter
+from sys import getsizeof
 
-def search(content, data, module):
-    if data > content[content]
+
+def search(content, date, module, i):
+    l = 0
+    r = len(content) - 1
+    response = []
+
+    while l <= r:
+        mid = int((l + r) / 2)
+
+        if i == 1:
+            d = date.hour
+            p = content[mid][0].hour
+        elif i == 2:
+            p = [content[mid][0].hour, content[mid][0].minute]
+            d = [date.hour, date.minute]
+        else:
+            p = [content[mid][0].hour, content[mid][0].minute, content[mid][0].second]
+            d = [date.hour, date.minute, date.second]
+        if d == p:
+            try:
+                content[mid][1][33:content[mid][1].find(']', 33)].index(module)
+            except ValueError:
+                pass
+            else:
+                response.append(content[mid][1])
+            content.pop(mid)
+            l = 0
+            r = len(content) - 1
+        elif date > content[mid][0]:
+            l = mid + 1
+        elif date < content[mid][0]:
+            r = mid - 1
+    return response
+
 
 def proc_request(data):
     date, module = data.split('-')
-
+    date = [int(i) for i in date.split(':')]
+    i = len(date)
+    date = datetime.time(*date)
+    return date, module, i
 
 
 def read_log(pth):
@@ -18,10 +55,12 @@ def read_log(pth):
     with open('{}'.format(pth), 'r') as f:
         for line in f.readlines():
             try:
-                content.append([line[:26], line[26:33], line[33:]])
-            except IndexError:
+                time_arr = [int(i) for i in line[12:19].split(':')]
+            except ValueError:
                 pass
-    return content
+            else:
+                content.append([datetime.time(time_arr[0], time_arr[1], time_arr[2]), line])
+    return sorted(content, key=itemgetter(0))
 
 
 sock = socket.socket()
@@ -30,13 +69,29 @@ sock.listen(5)
 conn, addr = sock.accept()
 print('Клиент подключился. IP ', addr)
 
-print(read_log('otrs_error.log')[0])
+content = read_log('otrs_error.log')
 
 while True:
     data = conn.recv(1024)
     if not data:
         break
-    response = proc_request(json.loads(data).decode())
+    date, module, i = proc_request(json.loads(data))
+    response = json.dumps(search(content, date, module, i)).encode('utf-8')
+    conn.send('{}'.format(getsizeof(response)).encode('utf-8'))
     conn.send(response)
 
 conn.close()
+
+# Результат
+# $ python3 task3_server.py &
+# [1] 1357
+# $ python3 task3_client.py
+# Введите строку в формате "HH[:MM[:SS]]-<some_string>" для поиска: 23:54-DB::CheckSessionID
+# Клиент подключился. IP  ('127.0.0.1', 48688)
+# Найдено 6 совпадений
+# [Sun Apr  2 23:54:54 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
+# [Sun Apr  2 23:54:49 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
+# [Sun Apr  2 23:54:46 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
+# [Sun Apr  2 23:54:24 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
+# [Sun Apr  2 23:54:18 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
+# [Sun Apr  2 23:54:15 2017][Error][Kernel::System::AuthSession::DB::CheckSessionID][49] Got no SessionID!!
