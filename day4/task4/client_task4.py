f0470c82c59f1c5dfdc01e05e99f45f1621c2be4
freedom_exists <freedomexists.pas@gmail.com>
2019-05-19 13:56:36 +0300

    refactoring (2) уже лучше

--- day4/task4/client_task4.py
+++ day4/task4/client_task4.py
@@ -1,72 +1,83 @@
 import sqlite3
 import socket
 
-conn = sqlite3.connect('some.db')
-c = conn.cursor()
-
 
 def main(sock):
+    conn = sqlite3.connect('some.db')
+    c = conn.cursor()
     while True:
         request = input('Введите запрос: ').split()
         method = request[0]
         if method == 'get':
             pth = request[1]
-            get_file(sock, pth)
-
+            filename = pth.split('/')[-1]
+            file = get_file(sock, pth)
+            if file:
+                c.execute('INSERT INTO files (filename, file) VALUES (?, ?)', (filename, sqlite3.Binary(file)))
+                conn.commit()
+                break
+            else:
+                break
         elif method == 'cp':
             _, dpth, lpth = request
             filename = dpth.split('/')[-1]
-
-            if r:
+            data = check_db(filename, c)
+            if data:
                 with open('{}'.format(lpth), 'wb') as cp_file:
-                    cp_file.write(r[1])
+                    cp_file.write(data[1])
                 break
             else:
-                get_request = 'get {}'.format(dpth)
-                sock.send(get_request.encode('utf-8'))
-                l = sock.recv(1024)
-                print(l)
+                sock.send(dpth.encode('utf-8'))
+                response = sock.recv(1024)
+                print(response)
                 with open('{}'.format(lpth), 'wb') as cp_file:
-                    while l:
-                        l = sock.recv(1024)
-                        cp_file.write(l)
+                    while response:
+                        response = sock.recv(1024)
+                        cp_file.write(response)
                 break
+    c.close()
+
 
 def get_file(sock, pth):
-    filename = pth.split('/')[-1]
     sock.send(pth.encode('utf-8'))
     response = sock.recv(1024).decode('utf-8')
     print(response)
     if response == 'Нет такого файла':
-        return
+        return None
     else:
         file = b''
         while response:
             response = sock.recv(1024)
             file += response
         print('Файл загружен')
-        c.execute('INSERT INTO files (filename, file) VALUES (?, ?)', (filename, sqlite3.Binary(file)))
-        conn.commit()
-        break
+        return file
 
 
-def check_db(filename):
+def check_db(filename, c):
     try:
-        c.execute('SELECT * FROM files WHERE filename = ?', (filename,))
+        c.execute('SELECT * FROM files WHERE filename = ?', (filename, ))
         file = c.fetchone()
     except sqlite3.DatabaseError as err:
         print('Ошибка', err)
+        return None
     else:
         return file
 
 
-sock = socket.socket()
-ip, port = input('Введите ip:port сервера: ').split(':')
-try:
-    sock.connect((ip, int(port)))
-except ConnectionError:
-    print('Ошибка адреса')
+def create_socket_connection():
+    sock = socket.socket()
+    while True:
+        ip, port = input('Введите ip:port сервера: ').split(':')
+        try:
+            sock.connect((ip, int(port)))
+        except ConnectionError:
+            print('Ошибка адреса')
+        else:
+            return sock
 
-main(sock)
-c.close()
+
+if __name__ == '__main__':
+    sock = create_socket_connection()
+    main(sock)
+
 
