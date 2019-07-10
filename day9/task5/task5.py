b7310b5444ab513f7a015265e0d3bae26f3428f0
freedom_exists <freedomexists.pas@gmail.com>
2019-07-10 20:44:14 +0300

    day9_task4 ну, допустим, done

--- day9/task5/task5.py
+++ day9/task5/task5.py
@@ -5,30 +5,45 @@ from jinja2 import Template
 
 def parse_content(content):
     content = content.decode('utf-8').replace('%3A', ':').replace('T', ' ')
-    parsed_content_list = []
     content = content.split('&')
+    if len(content) == 10:
+        parsed_content_list = []
+    else:
+        parsed_content_list = [None]
     for kv in content:
-        parsed_content_list.append(kv.split('=')[1])
-    print(parsed_content_list)
+        value = kv.split('=')[1]
+        if value == 'None' or value == '':
+            value = None
+        parsed_content_list.append(value)
     return tuple(parsed_content_list)
 
 
 def read_csv(csv_data):
     data = csv_data.decode('utf-8').split('\n')[5:-3]
-
     return data
 
 
 def insert_data(data):
     cursor = connect_db.cursor()
     add_data = ("INSERT INTO table_task1 "
-                "(servtype, subtype, user_id, referrer_user_id, "
+                "(service_id, servtype, subtype, user_id, referrer_user_id, "
                 "state, creation_date, creation_time, creation_request_sent_date,"
                 "notified_about_expiration)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                 ";")
 
     cursor.execute(add_data, data)
+    connect_db.commit()
+    cursor.close()
+
+
+def update_row(data):
+    cursor = connect_db.cursor()
+    update = ("UPDATE table_task1 SET servtype = %s, subtype = %s, user_id = %s, referrer_user_id = %s, "
+              "state = %s, creation_date = %s, creation_time = %s, creation_request_sent_date = %s, "
+              "notified_about_expiration = %s "
+              "WHERE service_id = {};".format(data[0]))
+    cursor.execute(update, data[1:])
     connect_db.commit()
     cursor.close()
 
@@ -66,7 +81,6 @@ class MyRequestHandler(BaseHTTPRequestHandler):
             del_row(pth[2])
         self.gen_page()
 
-
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
@@ -80,15 +94,20 @@ class MyRequestHandler(BaseHTTPRequestHandler):
             if data:
                 clear_table()
                 for row in data:
-                    row = row.split(';')[1:]
+                    row = row.split(';')
                     for i in range(len(row)):
                         if row[i] == 'NULL':
                             row[i] = None
                     row = tuple(row)
                     insert_data(row)
+        if self.path == '/update/':
+            data = parse_content(body)
+            try:
+                update_row(data)
+            except mysql.connector.errors.DataError:
+                pass
 
         self.gen_page()
-
 
     def gen_page(self):
         self.send_response(200)
@@ -97,6 +116,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         template = Template(html)
         table = get_data()
         self.wfile.write(template.render(table=table).encode())
+
 
 connect_db = mysql.connector.connect(user='blase', password='123',
                               host='127.0.0.1',
